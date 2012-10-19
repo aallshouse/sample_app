f8cf6867ba672183a0d65b41f6dad42badc0442f
aallshouse <aallshouse@ubuntu.(none)>
2012-10-18 22:28:03 -0400

    Add signout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,12 @@
 module SessionsHelper
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    self.current_user = user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 
   def signed_in?
