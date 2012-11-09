01f82c1ba5b42002d8838dd9075bbbd7fbd40de5
aallshouse <aallshouse@ubuntu.(none)>
2012-11-08 21:22:57 -0500

    Finish sign in

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -16,9 +16,9 @@ Given /^the user has an account$/ do
 end
 
 When /^the user submits valid signin information$/ do
-  fill_in "Email", with: @user.email
-  fill_in "Password", with: @user.password
-  click_button "Sign in"
+  fill_in 'Email', with: @user.email
+  fill_in 'Password', with: @user.password
+  click_button 'Sign in'
 end
 
 Then /^he should see his profile page$/ do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,35 @@
+
+include ApplicationHelper
+
 def full_title(page_title)
   base_title = 'Ruby on Rails Tutorial Sample App'
   if page_title.empty?
     base_title
   else
     "#{base_title} | #{page_title}"
+  end
+end
+
+def valid_signin(user)
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: user.password
+  click_button 'Sign in'
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_any_error_message do
+  match do |page|
+    page.should have_selector('div.alert.alert-error')
+  end
+end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text: title)
   end
 end
