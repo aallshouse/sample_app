bc3833088b47cf28d721d30c541a5fe214939466
Andrew Allshouse <aallshouse@matrixformedia.com>
2013-05-16 21:36:26 -0400

    Microposts 10.1 and 10.2 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,30 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
+  describe "micropost associations" do
+    before { @user.save }
+
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+  end
+
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
 
